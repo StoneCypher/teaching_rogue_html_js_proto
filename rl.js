640360dabfbc0464ac94618bad2f7b22dc538604
John Haugeland <stonecypher@gmail.com>
2015-06-21 09:51:53 -0700

    shows simple map with data in place

--- rl.js
+++ rl.js
@@ -5,14 +5,15 @@ var rl = (function(document) {
 
     function stringMapToTable(MapData, Width) {
 
-      var table = document.createElement('table'),
-          height = 20; // todo whargarbl
+      var table  = document.createElement('table'),
+          height = 20,  // todo whargarbl
+          idx    = 0;
 
       for (var j=0; j < height; ++j) {
         var tr = document.createElement('tr');
         for (var i=0; i < Width; ++i) {
           var td = document.createElement('td');
-          td.innerHTML = '.';
+          td.innerHTML = MapData[idx++];
           tr.appendChild(td);
         }
         table.appendChild(tr);
@@ -49,4 +50,4 @@ var rl = (function(document) {
 
 })(document);
 
-rl.renderString('', 40);
+rl.renderString(thisMap, 40);
