73fa1135251934ba7e0ff8a71c2acc7947f8d51e
John Haugeland <stonecypher@gmail.com>
2015-06-21 14:48:16 -0700

    stub root control

---
+++ src/jsx/tr.jsx
@@ -0,0 +1,15 @@
+
+var React  = require('react'),
+    Flocks = require('flocks');
+
+
+
+
+
+module.exports = Flocks.createClass({
+
+  render: function() {
+    return <div>Hello, world!</div>;
+  }
+
+});
